b7b72ca2af56f06593c9f8d044446b3cfc65bb89
Nayibe del Carmen Fajardo Silva <23470289@campeche.tecnm.mx>
2026-05-29 10:21:46 -0600

    Agrega Proyectores a la base de datos

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Agregar el contexto de la base de datos al contenedor de servicios
+builder.Services.AddDbContext<AppDbContext>(
+    //options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"))
+    options => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Proyectores;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30")
+    );
+
+builder.Services.AddTransient<IProyectoresService, ProyectoresService>();
 
 var app = builder.Build();
 
---
+++ WebApp/Services/ProyectoresService.cs
@@ -0,0 +1,29 @@
+﻿using WebApp.Models;
+using WebApp.Data;
+
+namespace WebApp.Services
+{
+    public class ProyectoresService : IProyectoresService
+    {
+        private readonly AppDbContext _contexto;
+
+        public ProyectoresService(AppDbContext contexto)
+        {
+            this._contexto = contexto;
+        }
+
+        public void AddProyector(Proyector proyector)
+        {
+            if(proyector != null)
+            {
+                _contexto.Proyectores.Add(proyector);
+                _contexto.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Proyector> GetAll()
+        {
+            return _contexto.Proyectores.ToList();
+        }
+    }
+}
